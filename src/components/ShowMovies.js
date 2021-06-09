ab7f7bb5121ecaa4673ffc1167df35357c8c6e2a
camilla-bachna <camilla.bachna@gmail.com>
2021-06-09 21:35:06 +0200

    add delete function

--- src/components/ShowMovies.js
+++ src/components/ShowMovies.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { db } from './Firebase';
 
 function ShowMovies(props) {
   const moviesToShow = props.dataToShow.map((data) => {
@@ -6,23 +6,64 @@ function ShowMovies(props) {
       <li className="container-list">
         <h3 className="container-heading"> {data.title}</h3>
         <p className="container-paragraph">{data.description}</p>
+        <p>{data.id}</p>
         <span className="container-relatedMovies">
           Películas relacionadas: {data.relatedMovies}
         </span>
+        <button onClick={() => deleteMovie(data)} className="container-delete">
+          Eliminar
+        </button>
+        <button
+          className="container-delete"
+          onClick={() =>
+            editMovie({
+              title: data.title,
+              description: data.description,
+              relatedMovies: data.relatedMovies,
+              id: data.id,
+            })
+          }
+        >
+          Modificar
+        </button>
       </li>
     );
   });
 
+  //delete movie function
+
+  const ref = db.collection('movies');
+
+  function deleteMovie(movie) {
+    ref
+      .doc(movie.id)
+      .delete()
+      .catch((error) => {
+        alert(error.message);
+      });
+    console.log(movie.id);
+  }
+
+  // edit movie function
+
+  function editMovie(updatedMovie) {
+    ref
+      .doc(updatedMovie.id)
+      .update(updatedMovie)
+      .then(() => {
+        alert('La película ha sido cambiada');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   return (
     <>
-      <h2> Hola! </h2>
       <h3>
         Aqui puedes ver todos los pelícilas añadidas y editarlos o eliminarlos
       </h3>
       <ul className="container">{moviesToShow}</ul>
-      <Link to="/" className="link">
-        <h4 className="form-link">Volver a la página principal</h4>
-      </Link>
     </>
   );
 }
